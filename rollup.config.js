590f611ecd17be894e67ab0bd05b112eb8f72958
ycycwx <yytcjcy@gmail.com>
2021-10-18 11:27:21 +0800

    chore: update rollup babel plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,16 @@
  * @author ycy
  */
 
-import babel from 'rollup-plugin-babel';
+// @ts-check
+
+import babel from '@rollup/plugin-babel';
 import {terser} from 'rollup-plugin-terser';
 
 export default {
     plugins: [
         babel({
             babelrc: false,
+            babelHelpers: 'bundled',
             presets: [['@babel/preset-env', {loose: true, modules: false}]]
         }),
         terser()
