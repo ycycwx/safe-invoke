dde669d362459fc692b6d8068737204762b80553
ycycwx <yytcjcy@gmail.com>
2024-09-03 23:52:26 +0800

    chore: add exports field to package.json for better module resolution

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,15 +3,31 @@
  * @author ycy
  */
 
-// @ts-check
-
 import terser from '@rollup/plugin-terser';
+import pkg from './package.json' with {type: 'json'};
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default {
-    plugins: [
-        terser()
-    ]
+    input: './src/index.js',
+    output: [
+        {
+            file: pkg.exports['.'].require,
+            format: 'cjs',
+            sourcemap: true,
+        },
+        {
+            file: pkg.exports['.'].import,
+            format: 'es',
+            sourcemap: true,
+        },
+        {
+            file: pkg.exports['.'].default,
+            format: 'umd',
+            name: 'safeInvoke',
+            sourcemap: true,
+        },
+    ],
+    plugins: [terser()],
 };
